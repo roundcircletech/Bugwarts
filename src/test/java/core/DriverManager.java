eb77295bb6df7609f1d48e7c447b34412dffe44b
Uday Singh Chauhan <uday.chauhan@wyzard.ai>
2025-09-11 16:09:39 +0530

    Chrome Driver Push

--- src/test/java/core/DriverManager.java
+++ src/test/java/core/DriverManager.java
@@ -1,11 +1,14 @@
 package core;
 
+import org.apache.poi.ss.formula.atp.Switch;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.time.Duration;
+import java.util.Locale;
 
 public class DriverManager {
 
@@ -13,13 +16,24 @@ public class DriverManager {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--headless");
-            options.addArguments("--width=1920","--height=1080");
-            driver = new FirefoxDriver(options);
+            String browser =  System.getProperty("browser","firefox").toLowerCase();
+            switch (browser){
+                case "chrome":
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--headless=new"); // new headless mode
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
+
+                case "firefox":
+                default:
+                    FirefoxOptions options = new FirefoxOptions();
+                    options.addArguments("--headless");
+                    options.addArguments("--width=1920","--height=1080");
+                    driver = new FirefoxDriver(options);
+                    break;
+            }
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().window().maximize();
-//            System.out.println("Firefox Driver started");
         }
         return driver;
     }
