7581841a11e58a4798809870d91330f55d4270c3
Uday Singh Chauhan <uday.chauhan@wyzard.ai>
2025-09-13 19:34:58 +0530

    Driver Update

--- src/test/java/core/DriverManager.java
+++ src/test/java/core/DriverManager.java
@@ -14,15 +14,17 @@ public class DriverManager {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            String browser =  System.getProperty("browser","firefox").toLowerCase();
-            switch (browser){
+            String browser = System.getProperty("browser", "firefox").toLowerCase();
+            switch (browser) {
                 case "chrome":
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.addArguments("--headless=new");
                     chromeOptions.addArguments("--window-size=1920,1080");
                     chromeOptions.addArguments("--no-sandbox");
                     chromeOptions.addArguments("--disable-dev-shm-usage");
-                    chromeOptions.addArguments( "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36");
+                    chromeOptions.addArguments("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) " +
+                            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36");
+                    chromeOptions.setAcceptInsecureCerts(true);
                     driver = new ChromeDriver(chromeOptions);
                     break;
 
@@ -30,7 +32,8 @@ public class DriverManager {
                 default:
                     FirefoxOptions options = new FirefoxOptions();
                     options.addArguments("--headless");
-                    options.addArguments("--width=1920","--height=1080");
+                    options.addArguments("--width=1920", "--height=1080");
+                    options.setAcceptInsecureCerts(true);
                     driver = new FirefoxDriver(options);
                     break;
             }
